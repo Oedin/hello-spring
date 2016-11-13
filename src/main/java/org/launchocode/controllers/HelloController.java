5cdcbf643a7567d962bfdb2250c4bbd35b1f9de5
EdinO <osmedin@gmail.com>
2016-11-12 21:52:22 -0600

    added thymeleaf dependencies to pom and exmpl

--- src/main/java/org/launchocode/controllers/HelloController.java
+++ src/main/java/org/launchocode/controllers/HelloController.java
@@ -4,22 +4,28 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.launchocode.models.HelloMessage;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
+	
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	@ResponseBody
-	public String hello(HttpServletRequest request){
+	public String helloForm(){
+		return "helloform";
+	}
+
+	@RequestMapping(value = "/hello", method = RequestMethod.POST)
+	public String hello(HttpServletRequest request, Model model){
 		
 		// get name name parameter from request will be null if there is no parameter
 		String name = request.getParameter("name");
-		if(name==null){
+		if(name==null || name==""){
 			name = "world";
 		}
-		return "<h1>" +HelloMessage.getMessage(name)+"</h1>";
+		model.addAttribute("name", name);
+		return "hello";
 	}
 }
